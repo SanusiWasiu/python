1ba85014b6ea26d5771ee58cd1bf63df5fcf3daa
Sanusi Wasiu <52449642+SanusiWasiu@users.noreply.github.com>
2020-05-01 16:27:21 +0100

    Add files via upload

---
+++ Beginner's luck.py
@@ -0,0 +1,7 @@
+def is_perfect_square(number):
+    sqrt = int(number**0.5)
+    print(sqrt**2 == number)
+is_perfect_square(9)
+is_perfect_square(100)
+is_perfect_square(225)
+is_perfect_square(500)
